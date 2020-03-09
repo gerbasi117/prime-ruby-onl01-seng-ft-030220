c3617ad360c3b5b00412f3ad3ff693e5a5d0356b
Brandon Gerbasi <brandon.gerbasi@gmail.com>
2020-03-09 02:34:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,3 @@
-def prime?(integer)
-  i = 2 
-  return false if integer is <= 1
-  while i < integer
-  return false if integer % i == 0 
-   i += 1 
- end
-true
-end
-
 def get_prime_no_upto(number)
   start = 2
   primes = (start..number).to_a
